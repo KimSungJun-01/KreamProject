1e3c7d6c3d482bbf33b6dc7e7b9b469017bb8a25
DEVELOPER\moonh <jt2302@naver.com>
2024-11-14 16:15:00 +0900

    초기환경 setting KimSungJun-01 #1

---
+++ src/main/java/com/kream/home/HomeController.java
@@ -0,0 +1,5 @@
+package com.kream.home;
+
+public class HomeController {
+
+}
--- src/main/java/com/kream/test/TestController.java
+++ src/main/java/com/kream/test/TestController.java
@@ -1,14 +1,48 @@
 package com.kream.test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kream.user.bo.UserBO;
+import com.kream.user.domain.User;
 
 @Controller
 public class TestController {
+	
+	@Autowired
+	private UserBO userBO;
+	
 	@ResponseBody
-		@RequestMapping("/test1")
-		public String helloWorld() {
-			return "Hello world!";
+	@GetMapping("/test1")
+	public String test1() {
+		return "<h2>Hello world</h2>";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test2")
+	public Map<String, Object> test2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("a", 1);
+		map.put("b", 2);
+		map.put("c", 3);
+		return map;
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test3";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test4")
+	public List<User> test4() {
+		System.out.println(userBO.getUserList());
+		return userBO.getUserList();
 	}
 }
